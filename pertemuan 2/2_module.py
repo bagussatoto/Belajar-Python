35021f5cfad38cd500e94737dd0e47d062655f96
BAGUS BUDI SATOTO <79775593+bagussatoto1@users.noreply.github.com>
2024-01-04 19:38:09 +0700

    create : ``pertemuan 2``

---
+++ pertemuan 2/2_module.py
@@ -0,0 +1,24 @@
+# APa itu Module
+# Module adalah sebuah file yang berisi kode program Python
+# Module dapat digunakan kembali
+# Module dapat berisi fungsi, class, atau variabel
+
+# # Membuat Module
+# # ## Membuat file baru dengan ekstensi .py
+# # ## Tulis kode program Python
+# # ## Import module
+# import myModule
+#
+
+# # Membuat Module
+# # ## Membuat file baru dengan ekstensi .py
+# # ## Tulis kode program Python
+# # ## Import module
+# import myModule
+#
+# # Menggunakan Module
+# myModule.myFunction()
+# print(myModule.myVariable)
+# print(myModule.myClass().name)
+# print(myModule.myClass().age)
+# 
