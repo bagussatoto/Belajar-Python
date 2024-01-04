4817b1f430516e3703a5f7a539a38720e6d212c6
BAGUS BUDI SATOTO <79775593+bagussatoto1@users.noreply.github.com>
2024-01-04 19:38:41 +0700

    cretae : ``pertemuan 2``

---
+++ pertemuan 2/main.py
@@ -0,0 +1,14 @@
+
+
+
+# import books as bk
+# from module.database import books
+from module import database
+
+database.books.insertBook("Buku 1", "Author 1")
+database.books.showBooks()
+
+# module.database.books.showBooks()
+# module.database.books.insertBook("Buku 2", "Author 2")
+
+# module.database.books.showBooks()
