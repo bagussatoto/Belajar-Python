3be29c72383d11827ea5640885b5baaa22eb7d3a
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2024-01-05 09:49:18 +0700

    pertemuan 5

---
+++ pertemuan 5/tips/eval.py
@@ -0,0 +1,32 @@
+
+
+# aritmatika = ['+', '-', '*', '/']
+# operation = ['tambah', 'kurang', 'kali', 'bagi']
+
+# x = "10"
+# y = "20"
+
+# print(aritmatika[1])
+# print(aritmatika[operation.index('kali')])
+
+# # 10 * 20
+# hasil = x+aritmatika[operation.index('kali')]+ y
+
+# result = eval(hasil)
+
+# print(hasil)
+# print(result)
+
+# # statement = input("Masukkan perintah: ")
+# # eval(statement)
+
+
+eval(("""
+def tambah(x, y):
+    return x + y
+
+
+print(tambah(10, 20))     
+
+
+"""))
